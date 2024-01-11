0ffd7ac51c0e82fdc65cfc5a3dbe07b36d476a7e
nmarks99 <nmarks@anl.gov>
2024-01-11 11:12:29 -0600

    using smart pointer for RTDEReceiveInterface obj
    also formatted with LLVM style, 4 space indents and
    added the .clang-format file to the project root

--- urRobotApp/src/ur_driver.cpp
+++ urRobotApp/src/ur_driver.cpp
@@ -1,104 +1,103 @@
-#include <iostream>
-#include <iocsh.h>
+#include "ur_driver.hpp"
+
+#include <asynOctetSyncIO.h>
 #include <epicsExport.h>
 #include <epicsString.h>
 #include <epicsThread.h>
-#include <asynOctetSyncIO.h>
-#include "ur_driver.hpp"
+#include <iocsh.h>
 
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+
+#include "ur_rtde/rtde_receive_interface.h"
 
 static void main_loop_thread_C(void *pPvt) {
-    URRobot *pURRobot = (URRobot*)pPvt;
+    URRobot *pURRobot = (URRobot *)pPvt;
     pURRobot->main_loop();
 }
 
 // debugging function for development
-template<typename T>
-void print_vector(const std::vector<T>& vec, const std::string_view pref="", const std::string_view sep=",") {
+template <typename T>
+void print_vector(const std::vector<T> &vec, const std::string_view pref = "",
+                  const std::string_view sep = ",") {
     std::cout << pref;
-    for (const auto& element : vec) {
+    for (const auto &element : vec) {
         std::cout << element << sep;
     }
     std::cout << std::endl;
 }
 
-
-URRobot::URRobot(const char *asyn_port_name, const char* robot_ip) : asynPortDriver(asyn_port_name, MAX_CONTROLLERS,
-		asynInt32Mask | asynFloat64Mask | asynDrvUserMask | asynOctetMask,
-		asynInt32Mask | asynFloat64Mask | asynOctetMask,
-		ASYN_MULTIDEVICE | ASYN_CANBLOCK, 1, /* ASYN_CANBLOCK=0, ASYN_MULTIDEVICE=1, autoConnect=1 */
-		0, 0),
-    poll_time_(DEFAULT_POLL_TIME), count_(0), rtde_receive_(ur_rtde::RTDEReceiveInterface(robot_ip)), connected_(0)
-{
-   
+URRobot::URRobot(const char *asyn_port_name, const char *robot_ip)
+    : asynPortDriver(asyn_port_name, MAX_CONTROLLERS,
+                     asynInt32Mask | asynFloat64Mask | asynDrvUserMask |
+                         asynOctetMask,
+                     asynInt32Mask | asynFloat64Mask | asynOctetMask,
+                     ASYN_MULTIDEVICE | ASYN_CANBLOCK,
+                     1, /* ASYN_CANBLOCK=0, ASYN_MULTIDEVICE=1, autoConnect=1 */
+                     0, 0),
+      rtde_receive_(std::make_unique<ur_rtde::RTDEReceiveInterface>(robot_ip)),
+      poll_time_(DEFAULT_POLL_TIME), count_(0), connected_(0) {
     // create asyn parameters
     createParam(IS_CONNECTED_STRING, asynParamInt32, &isConnectedIndex_);
 
-    // TODO: better way to do this?
-    if (rtde_receive_.isConnected()) {
-	std::cout << "Robot connected successfully" << std::endl;
-	setIntegerParam(isConnectedIndex_, 1);
+    // Check that robot is connected
+    if (rtde_receive_->isConnected()) {
+        std::cout << "Robot connected successfully" << std::endl;
+        setIntegerParam(isConnectedIndex_, 1);
+    } else {
+        std::cout << "Failed to connect to robot host " << robot_ip
+                  << std::endl;
+        setIntegerParam(isConnectedIndex_, 0);
+        throw std::runtime_error("Could not connect to robot");
     }
-    else {
-	std::cout << "Failed to connect to robot host " << robot_ip << std::endl;
-	setIntegerParam(isConnectedIndex_, 0);
-	// TODO: runtime error?
-    }
-    
-    // print the current pose
-    pose_ = rtde_receive_.getActualTCPPose();
+
+    pose_ = rtde_receive_->getActualTCPPose();
     print_vector(pose_, "Pose: ");
-    
+
     std::cout << "starting main loop..." << std::endl;
-    epicsThreadCreate(
-	"UrRobotMainLoop",
-	epicsThreadPriorityLow,
-	epicsThreadGetStackSize(epicsThreadStackMedium),
-	(EPICSTHREADFUNC)main_loop_thread_C,
-	this
-    );
-
+    epicsThreadCreate("UrRobotMainLoop", epicsThreadPriorityLow,
+                      epicsThreadGetStackSize(epicsThreadStackMedium),
+                      (EPICSTHREADFUNC)main_loop_thread_C, this);
 }
-
 
 void URRobot::main_loop() {
-
     while (true) {
-	lock();
+        lock();
 
-	// get the current pose and print it for testing
-	std::cout << "count = " << count_ << std::endl;	
-	count_ += 1;
-	pose_ = rtde_receive_.getActualTCPPose();
-	print_vector(pose_, "Pose: ");
+        std::cout << "count = " << count_ << std::endl;
+        count_ += 1;
 
-	callParamCallbacks();
+        if (rtde_receive_) {
+            pose_ = rtde_receive_->getActualTCPPose();
+            print_vector(pose_, "Pose: ");
+        }
 
-	unlock();
-	epicsThreadSleep(poll_time_);
+        callParamCallbacks();
+
+        unlock();
+        epicsThreadSleep(poll_time_);
     }
 }
 
-
-extern "C" int URRobotConfig(const char *asyn_port_name, const char* robot_ip) {
+// register function for iocsh
+extern "C" int URRobotConfig(const char *asyn_port_name, const char *robot_ip) {
     URRobot *pURRobot = new URRobot(asyn_port_name, robot_ip);
     pURRobot = NULL;
-    return(asynSuccess);
+    return (asynSuccess);
 }
 
 static const iocshArg urRobotArg0 = {"Asyn port name", iocshArgString};
 static const iocshArg urRobotArg1 = {"Robot IP address", iocshArgString};
-static const iocshArg * const urRobotArgs[2] = {&urRobotArg0, &urRobotArg1};
+static const iocshArg *const urRobotArgs[2] = {&urRobotArg0, &urRobotArg1};
 static const iocshFuncDef urRobotFuncDef = {"URRobotConfig", 2, urRobotArgs};
 
 static void urRobotCallFunc(const iocshArgBuf *args) {
     URRobotConfig(args[0].sval, args[1].sval);
 }
 
-void URRobotRegister(void) {
-    iocshRegister(&urRobotFuncDef, urRobotCallFunc);
-}
+void URRobotRegister(void) { iocshRegister(&urRobotFuncDef, urRobotCallFunc); }
 
 extern "C" {
-    epicsExportRegistrar(URRobotRegister);
+epicsExportRegistrar(URRobotRegister);
 }
--- urRobotApp/src/ur_driver.hpp
+++ urRobotApp/src/ur_driver.hpp
@@ -1,26 +1,27 @@
 #include <asynPortDriver.h>
+
 #include "ur_rtde/rtde_receive_interface.h"
 
 static constexpr char IS_CONNECTED_STRING[] = "IS_CONNECTED";
 
 constexpr int MAX_CONTROLLERS = 1;
-constexpr double DEFAULT_POLL_TIME = 0.20; // 5Hz?
+constexpr double DEFAULT_POLL_TIME = 0.20; // 5Hz
 
 #define DEFAULT_CONTROLLER_TIMEOUT 1.0
 
-class URRobot: public asynPortDriver {
-    public:
-	URRobot(const char *asyn_port_name, const char* robot_port_name);
-	virtual void main_loop(void);
+class URRobot : public asynPortDriver {
+  public:
+    URRobot(const char *asyn_port_name, const char *robot_port_name);
+    virtual void main_loop(void);
 
-    private:
-	ur_rtde::RTDEReceiveInterface rtde_receive_;
-	std::vector<double> pose_;
-	double poll_time_;
-	int count_;
-	int connected_;
+  private:
+    std::unique_ptr<ur_rtde::RTDEReceiveInterface> rtde_receive_;
+    std::vector<double> pose_;
+    double poll_time_;
+    int count_;
+    int connected_;
 
-    protected:
-	asynUser* pasynUserURRobot_;
-        int isConnectedIndex_;
+  protected:
+    asynUser *pasynUserURRobot_;
+    int isConnectedIndex_;
 };
