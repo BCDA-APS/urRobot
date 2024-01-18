d47272721bc837a295f1a98482b481ee95361b2e
nmarks99 <nmarks@anl.gov>
2024-01-18 17:38:01 -0600

    add new bob file for rtde status monitoring with rtde

--- iocs/ur621/start_phoebus.sh
+++ iocs/ur621/start_phoebus.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/APSshare/bin/phoebus -resource "file:/home/beams/NMARKS/devel/epics_ur_driver/support/urRobot/urRobotApp/op/bob/ur_dashboard.bob?P=ur621"
+/APSshare/bin/phoebus -nosplash -resource "file:/home/beams/NMARKS/devel/epics_ur_driver/support/urRobot/urRobotApp/op/bob/ur_dashboard.bob?P=ur621" -resource "file:/home/beams/NMARKS/devel/epics_ur_driver/support/urRobot/urRobotApp/op/bob/ur_rtde_status.bob?P=ur621"
