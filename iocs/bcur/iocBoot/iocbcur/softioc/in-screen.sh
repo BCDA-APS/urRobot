67a54de8399a95b5589939c3d4091fbf5d2e36b3
nmarks99 <nmarks@anl.gov>
2024-01-24 14:55:27 -0600

    changePrefix ur621->bcur

---
+++ iocs/bcur/iocBoot/iocbcur/softioc/in-screen.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# $Id$
+
+/usr/bin/screen -dm -S bcur -h 5000 ./run
+
+# start the IOC in a screen session
+#  type:
+#   screen -r   to start interacting with the IOC command line
+#   ^a-d        to stop interacting with the IOC command line
+#   ^c          to stop the IOC
---
+++ iocs/bcur/iocBoot/iocbcur/urRobot.cmd
@@ -0,0 +1,11 @@
+epicsEnvSet("ROBOT_NAME", "BCDA:")
+
+# Set up UR RTDE interface
+epicsEnvSet("ASYN_PORT1", "ur_asyn1")
+dbLoadRecords("$(TOP)/../../db/rtde_receive.db", "P=$(PREFIX), R=$(ROBOT_NAME), PORT=$(ASYN_PORT1), ADDR=0")
+URRobotRTDEConfig("$(ASYN_PORT1)", "164.54.104.148")
+
+# Set up UR dashboard server
+epicsEnvSet("ASYN_PORT2", "ur_asyn2")
+dbLoadRecords("$(TOP)/../../db/dashboard.db", "P=$(PREFIX), R=$(ROBOT_NAME), PORT=$(ASYN_PORT2), ADDR=0")
+URRobotDashboardConfig("$(ASYN_PORT2)", "164.54.104.148")
