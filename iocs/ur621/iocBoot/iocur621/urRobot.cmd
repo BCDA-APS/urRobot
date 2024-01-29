826879283262dd9da2b7fa2479e4ec0a5f33d417
nmarks99 <nmarks@anl.gov>
2024-01-29 11:44:46 -0600

    remove gixs stuff

--- iocs/ur621/iocBoot/iocur621/urRobot.cmd
+++ iocs/ur621/iocBoot/iocur621/urRobot.cmd
@@ -3,9 +3,9 @@ epicsEnvSet("ROBOT_NAME", "")
 # Set up UR RTDE interface
 epicsEnvSet("ASYN_PORT1", "ur_asyn1")
 dbLoadRecords("$(TOP)/../../db/rtde_receive.db", "P=$(PREFIX), R=$(ROBOT_NAME), PORT=$(ASYN_PORT1), ADDR=0")
-URRobotRTDEConfig("$(ASYN_PORT1)", "164.54.104.148")
+URRobotRTDEConfig("$(ASYN_PORT1)", "164.54.104.0")
 
 # Set up UR dashboard server
-epicsEnvSet("ASYN_PORT2", "ur_asyn2")
-dbLoadRecords("$(TOP)/../../db/dashboard.db", "P=$(PREFIX), R=$(ROBOT_NAME), PORT=$(ASYN_PORT2), ADDR=0")
-URRobotDashboardConfig("$(ASYN_PORT2)", "164.54.104.148")
+# epicsEnvSet("ASYN_PORT2", "ur_asyn2")
+# dbLoadRecords("$(TOP)/../../db/dashboard.db", "P=$(PREFIX), R=$(ROBOT_NAME), PORT=$(ASYN_PORT2), ADDR=0")
+# URRobotDashboardConfig("$(ASYN_PORT2)", "164.54.104.148")
