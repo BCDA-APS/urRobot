39676b55887657c993ab2be9719098be25b90a40
nmarks99 <nmarks@anl.gov>
2024-07-10 13:06:42 -0500

    add action to waypointL.db

--- urRobotApp/src/action_opt.lua
+++ urRobotApp/src/action_opt.lua
@@ -9,16 +9,16 @@ local Actions = {
 -- Sets the ActionDoneCalc for the waypoint for one of the preset actions above
 function select(args)
 
-    local action = string.format("%s%sWaypointJ:%d:Action.FLNK", args.P, args.R, args.N)
-    local inpa = string.format("%s%sWaypointJ:%d:ActionDoneCalc.INPA", args.P, args.R, args.N)
-    local inpb = string.format("%s%sWaypointJ:%d:ActionDoneCalc.INPB", args.P, args.R, args.N)
-    local inpc = string.format("%s%sWaypointJ:%d:ActionDoneCalc.INPC", args.P, args.R, args.N)
-    local inpd = string.format("%s%sWaypointJ:%d:ActionDoneCalc.INPD", args.P, args.R, args.N)
-    local vala = string.format("%s%sWaypointJ:%d:ActionDoneCalc.A", args.P, args.R, args.N)
-    local valb = string.format("%s%sWaypointJ:%d:ActionDoneCalc.B", args.P, args.R, args.N)
-    local valc = string.format("%s%sWaypointJ:%d:ActionDoneCalc.C", args.P, args.R, args.N)
-    local vald = string.format("%s%sWaypointJ:%d:ActionDoneCalc.D", args.P, args.R, args.N)
-    local calc = string.format("%s%sWaypointJ:%d:ActionDoneCalc.CALC", args.P, args.R, args.N)
+    local action = string.format("%s%sWaypoint%s:%d:Action.FLNK", args.P, args.R, args.T, args.N)
+    local inpa = string.format("%s%sWaypoint%s:%d:ActionDoneCalc.INPA", args.P, args.R, args.T, args.N)
+    local inpb = string.format("%s%sWaypoint%s:%d:ActionDoneCalc.INPB", args.P, args.R, args.T, args.N)
+    local inpc = string.format("%s%sWaypoint%s:%d:ActionDoneCalc.INPC", args.P, args.R, args.T, args.N)
+    local inpd = string.format("%s%sWaypoint%s:%d:ActionDoneCalc.INPD", args.P, args.R, args.T, args.N)
+    local vala = string.format("%s%sWaypoint%s:%d:ActionDoneCalc.A", args.P, args.R, args.T, args.N)
+    local valb = string.format("%s%sWaypoint%s:%d:ActionDoneCalc.B", args.P, args.R, args.T, args.N)
+    local valc = string.format("%s%sWaypoint%s:%d:ActionDoneCalc.C", args.P, args.R, args.T, args.N)
+    local vald = string.format("%s%sWaypoint%s:%d:ActionDoneCalc.D", args.P, args.R, args.T, args.N)
+    local calc = string.format("%s%sWaypoint%s:%d:ActionDoneCalc.CALC", args.P, args.R, args.T, args.N)
 
     if (A == Actions.OpenRobotiqGripper) then
         local open = string.format("%s%sRobotiqGripper:Open.PROC", args.P, args.R)
